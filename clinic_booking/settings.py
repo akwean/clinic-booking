2e3448965287041b079c10312bbb212613983012
cj-aquin <regrowth1521@gmail.com>
2025-02-20 15:41:02 +0800

    railway2.4

--- clinic_booking/settings.py
+++ clinic_booking/settings.py
@@ -55,12 +55,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'clinic_booking.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
-}
+ DATABASES = {
+      'default': dj_database_url.config(
+          default=os.environ.get('DATABASE_URL')
+      )
+  }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
